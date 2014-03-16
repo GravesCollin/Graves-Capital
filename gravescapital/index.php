2f6c66e8e956f98fc476f282ff055b98cd6e2aa3
Collin Graves <collin.graves@yahoo.com>
2014-03-15 23:32:29 -0500

    Initialized Build

---
+++ gravescapital/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Graves Capital</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/mystyle.css">
+	<script src="js/myscript.js" type="text/javascript"></script>
+</head>
+
+
+
+<body>
+	
+</body>
+</html>
